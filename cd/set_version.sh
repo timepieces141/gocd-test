1c40e66e868ac7708277c5e43eac85caae734bbd
Edward Petersen <edward.petersen@gmail.com>
2014-08-13 02:23:22 -0400

    dash instead of underscore in version number (maven seems to care); added versions maven plugin to pom.xml

--- cd/set_version.sh
+++ cd/set_version.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 
-mvn versions:set -DnewVersion=0.0.1_$GO_PIPELINE_COUNTER
-
+mvn versions:set -DnewVersion=0.0.1-$GO_PIPELINE_COUNTER
--- cd/tag.sh
+++ cd/tag.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
-git tag 0.0.1_$GO_PIPELINE_COUNTER
-git checkout 0.0.1_$GO_PIPELINE_COUNTER
-
+git tag 0.0.1-$GO_PIPELINE_COUNTER
+git checkout 0.0.1-$GO_PIPELINE_COUNTER
