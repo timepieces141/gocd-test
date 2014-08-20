37fd971cc42bcfeb7a9a612b8a125c472ba729bb
Edward Petersen <edward.petersen@gmail.com>
2014-08-20 17:19:26 -0400

    apparently you can't push unless user.email and user.name are set - we set them, non-globally, each time

--- scripts/push_new.sh
+++ scripts/push_new.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 cd $1
+git config user.email "gocd@px.ftw"
+git config user.name "GoCD Server"
 git add toplevelpackage/*
 git commit -a -m "Generated code from build #$GO_PIPELINE_COUNT"
 git push origin master
