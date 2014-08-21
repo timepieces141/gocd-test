bda093b8e74fc2c4966685d651b0c8de72d76b2d
Edward Petersen <edward.petersen@gmail.com>
2014-08-20 22:34:57 -0400

    change the .git/config file in the go output directory to use ssh for the push

--- scripts/push_new.sh
+++ scripts/push_new.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
 cd $1
+sed -i 's/https:\/\//git@/g' .git/config
+sed -i 's/\/timepieces141/:timepieces141/g' .git/config
+sed -i 's/gen-code/gen-code.git/g' .git/config
 git config user.email "gocd@px.ftw"
 git config user.name "GoCD Server"
 git add toplevelpackage/*
 git commit -a -m "Generated code from build #$GO_PIPELINE_COUNT"
 git push origin master
-
